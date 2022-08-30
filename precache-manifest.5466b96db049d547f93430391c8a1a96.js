8c7d421c5eb44dff59a309b1886c86e4f0734241
mimi-kyu <mimi.prangova@gmail.com>
2022-08-30 10:33:39 +0200

    Updates

---
+++ precache-manifest.5466b96db049d547f93430391c8a1a96.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "0d425c1684ee7aa9ed8f",
+    "url": "/react-crud/static/css/main.235cfd04.chunk.css"
+  },
+  {
+    "revision": "0d425c1684ee7aa9ed8f",
+    "url": "/react-crud/static/js/main.0d425c16.chunk.js"
+  },
+  {
+    "revision": "ce47ab81b35290e2776b",
+    "url": "/react-crud/static/js/1.ce47ab81.chunk.js"
+  },
+  {
+    "revision": "23c968ea8243ea2c4882",
+    "url": "/react-crud/static/css/2.5cb102c2.chunk.css"
+  },
+  {
+    "revision": "23c968ea8243ea2c4882",
+    "url": "/react-crud/static/js/2.23c968ea.chunk.js"
+  },
+  {
+    "revision": "16c417008d3e440ed25f",
+    "url": "/react-crud/static/js/runtime~main.16c41700.js"
+  },
+  {
+    "revision": "2d106dd45b2cb09346908426e04615c2",
+    "url": "/react-crud/index.html"
+  }
+];
