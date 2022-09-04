59ac1b44f5ee387c30376dba4ae2909035a078c2
mimi-kyu <mimi.prangova@gmail.com>
2022-09-04 15:58:37 +0200

    Revert "Added cors proxy to base URL."
    This reverts commit afb6e85b790dc7f0bca9d8f41ba57e33ae9c7a71.
    
    Removed cors proxy from baseURL

--- src/http-common.js
+++ src/http-common.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 export default axios.create({
-  baseURL: "https://cors-everywhere-me.herokuapp.com/http://spring-demo-jpa-dev.us-east-1.elasticbeanstalk.com/api",
+  baseURL: "http://spring-demo-jpa-dev.us-east-1.elasticbeanstalk.com/api",
   headers: {
     "Content-type": "application/json"
   }
