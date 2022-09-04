ad1c1680a0a40eb05d2bad002e45e38bf0817bee
mimi-kyu <mimi.prangova@gmail.com>
2022-09-04 15:03:08 +0200

    Deploy React app to GitHub Pages after adding cors proxy.

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-crud/precache-manifest.819a88334d6d3674286e4ebd51fdf3a3.js"
+  "/react-crud/precache-manifest.0547b7591c0469054520176df61814d8.js"
 );
 
 workbox.clientsClaim();
