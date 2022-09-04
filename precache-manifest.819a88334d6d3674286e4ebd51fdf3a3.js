fedb7b730301727a68c215bcdd6ddb1278af5946
mimi-kyu <mimi.prangova@gmail.com>
2022-09-04 16:07:08 +0200

    Deploy React app to GitHub Pages after removing cors proxy.

--- precache-manifest.819a88334d6d3674286e4ebd51fdf3a3.js
+++ precache-manifest.819a88334d6d3674286e4ebd51fdf3a3.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "032458ea64544bcab55f",
+    "revision": "3e8e46ffcf00411d23bd",
     "url": "/react-crud/static/css/main.235cfd04.chunk.css"
   },
   {
-    "revision": "032458ea64544bcab55f",
-    "url": "/react-crud/static/js/main.032458ea.chunk.js"
+    "revision": "3e8e46ffcf00411d23bd",
+    "url": "/react-crud/static/js/main.3e8e46ff.chunk.js"
   },
   {
     "revision": "ce47ab81b35290e2776b",
@@ -24,7 +24,7 @@ self.__precacheManifest = [
     "url": "/react-crud/static/js/runtime~main.16c41700.js"
   },
   {
-    "revision": "9cb15f904a08c5ee07a98b40a137448c",
+    "revision": "5a9246ded7dd8ed4db235a8762e69c46",
     "url": "/react-crud/index.html"
   }
 ];
