b6916fe241fc5c2e006a61f1b236c8d230629b09
mimi-kyu <mimi.prangova@gmail.com>
2022-09-05 10:53:28 +0200

    Added api to the end of the baseURL.

--- src/http-common.js
+++ src/http-common.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 export default axios.create({
-  baseURL: "https://d4xw46zvf5.execute-api.us-east-1.amazonaws.com",
+  baseURL: "https://d4xw46zvf5.execute-api.us-east-1.amazonaws.com/api",
   headers: {
     "Content-type": "application/json"
   }
