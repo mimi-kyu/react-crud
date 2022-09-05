58c0ae12f5f0e85fbe0081bc599ed015b59484bd
mimi-kyu <mimi.prangova@gmail.com>
2022-09-05 09:22:10 +0200

    Set header X-Requested-With with value  XMLHttpRequest.

--- src/http-common.js
+++ src/http-common.js
@@ -2,6 +2,7 @@ import axios from "axios";
 export default axios.create({
   baseURL: "http://spring-demo-jpa-dev.us-east-1.elasticbeanstalk.com/api",
   headers: {
-    "Content-type": "application/json"
+    "Content-type": "application/json",
+    "X-Requested-With": "XMLHttpRequest"
   }
 });
