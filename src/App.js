1c927033709bfb70c7d8366a031bc13067bde2ea
mimi-kyu <mimi.prangova@gmail.com>
2022-09-07 15:14:11 +0200

    index added to the Route

--- src/App.js
+++ src/App.js
@@ -34,10 +34,10 @@ class App extends Component{
       </nav>
       <div className="container mt-3">
         <Routes>
-          <Route path="/" element={<TutorialsList />} />
-          <Route path="add" element={<AddTutorial />} />
-          <Route path="tutorials" element={<TutorialsList />} />
-          <Route path="tutorials/:id" element={<Wrapper />} />
+          <Route index element={<TutorialsList />} />
+          <Route path="/add" element={<AddTutorial />} />
+          <Route path="/tutorials" element={<TutorialsList />} />
+          <Route path="/tutorials/:id" element={<Wrapper />} />
         </Routes>
       </div>
     </div>);
