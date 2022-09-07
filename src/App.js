13ef6db5fdbbdcbec0cc154cdf1d495172b940f3
mimi-kyu <mimi.prangova@gmail.com>
2022-09-07 16:17:49 +0200

    Changed Route Link to attribute to use relative path.

--- src/App.js
+++ src/App.js
@@ -16,17 +16,17 @@ class App extends Component{
     return (
       <div>
       <nav className="navbar navbar-expand navbar-dark bg-dark">
-        <Link to={"/tutorials"} className="navbar-brand">
+        <Link to={"tutorials"} className="navbar-brand">
           Mimi's Tutorials
         </Link>
         <div className="navbar-nav mr-auto">
           <li className="nav-item">
-            <Link to={"/tutorials"} className="nav-link">
+            <Link to={"tutorials"} className="nav-link">
               Tutorials
             </Link>
           </li>
           <li className="nav-item">
-            <Link to={"/add"} className="nav-link">
+            <Link to={"add"} className="nav-link">
               Add
             </Link>
           </li>
@@ -35,9 +35,9 @@ class App extends Component{
       <div className="container mt-3">
         <Routes>
           <Route path="*" element={<TutorialsList />} />
-          <Route path="add" element={<AddTutorial />} />
-          <Route path="tutorials" element={<TutorialsList />} />
-          <Route path="tutorials/:id" element={<Wrapper />} />
+          <Route path="/add" element={<AddTutorial />} />
+          <Route path="/tutorials" element={<TutorialsList />} />
+          <Route path="/tutorials/:id" element={<Wrapper />} />
         </Routes>
       </div>
     </div>);
